dcc91ed404de22422f21de472ef68ef0b47a6d05
Diegomau182 <Diegomauri2013@gmail.com>
2020-03-12 18:34:25 -0600

    Agrega un nueva variable a laa tupla

--- messages.py
+++ messages.py
@@ -1,13 +1,15 @@
 #-*-utf-8-*-
 import datetime
 
+#Declara una variable global
+id_message = False
 
 class Messages:
     '''
     Simula un mensaje enviado a un usuario como
     un serivicio de mensajeria
     '''
-    def __init__(self, from_number="", text_of_sms=""):
+    def __init__(self, from_number="", text_of_sms="",has_been_viewed=""):
         """
         Inicializa un mensaje con el valores de from_number,
          time_arrived, text_of_sms enviadas por el usuario
@@ -15,8 +17,13 @@ class Messages:
         """
         self.from_number = from_number
         self.text_of_sms = text_of_sms
+        self.has_been_viewed = has_been_viewed
         self.create_time = datetime.date.today()
-        self.visto = 0
+        global id_message
+        id_message += 1
+        self.id = id_message
+    
+
 
     
 
