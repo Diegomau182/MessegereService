4d13d673fd81621f59341d846e885e824591c600
Diegomau182 <Diegomauri2013@gmail.com>
2020-03-12 18:37:43 -0600

    Comienza el menu desde cero

---
+++ menu.py
@@ -0,0 +1,10 @@
+# -*- coding: utf8 -*-
+import sys
+import platform
+from service_message import ShortMessageService
+
+
+class Menu:
+    """
+    Despliega un menú que responde a las elecciones del usuario.
+    """
